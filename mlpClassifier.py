0842ea04b57ac9d0f7c7f84220f8e2bdf15e9363
Takang Kajikaw Etta Tabe <tabe@Takangs-MacBook-Air.local>
2018-05-16 04:16:47 +0200

    fix NA bug in training and test data

--- mlpClassifier.py
+++ mlpClassifier.py
@@ -19,34 +19,48 @@ data['card_id'] = [x.strip().replace('card', '') for x in data['card_id']]
 data['ip_id'] = [x.strip().replace('ip', '') for x in data['ip_id']]
 data['mail_id'] = [x.strip().replace('email', '') for x in data['mail_id']]
 
-'''Creating training, dev and test sets'''
-# columns = "txid bookingdate issuercountrycode txvariantcode bin amount currencycode shoppercountrycode shopperinteraction cardverificationcodesupplied" \
-#           " cvcresponsecode creationdate accountcode mail_id ip_id card_id ".split()
-
+'''Create dummy values for columns with strings as values'''
 data = pd.get_dummies(data, columns=['txvariantcode', 'currencycode', 'shopperinteraction', 'issuercountrycode',
                                        'cardverificationcodesupplied', 'shoppercountrycode', 'currencycode',
                                       'cvcresponsecode', 'accountcode'], drop_first=True)
-data.drop(data.columns[[ 1, 5]], axis=1, inplace=True)
+
+'''Drop the txid, booking and creationdate columns'''
+data.drop(data.columns[[0, 1, 5]], axis=1, inplace=True)
+
+
+'''Normalize amount feature for transactions'''
+data['normalamount'] = StandardScaler().fit_transform(data['amount'].reshape(-1,1))
+data = data.drop(['amount'], axis=1)
 
 X = pd.DataFrame.as_matrix(data)
 y = data.simple_journal
 y = y.reshape(y.shape[0],1)
+X.shape
 
 '''Split entire dataset and the undersampled data into training and test sets'''
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.06)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.06, random_state=0)
 
 '''Classifier'''
 classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5,2), random_state=1)
 
+X_train = np.nan_to_num(X_train)
+for i,item in enumerate(X_train):
+    if "NA" in item:
+        X_train[i] = 0
 
-X_train = X_train.astype(int)
-X_test= X_test.astype(int)
+for i,item in enumerate(X_test):
+    if "NA" in item:
+        X_test[i] = 0
 
+
+X_train = np.nan_to_num(X_train)
+y_train = y_train.astype(int)
+X_test = np.nan_to_num(X_test)
 classifier.fit(X_train, y_train)
 
 
 
-pred = classifier.predict( X_test)
+pred = classifier.predict(X_test)
 
 confusionmatrix_undersample = confusion_matrix(y_test, pred)
 
